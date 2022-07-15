6a6e7e62882f00e8bbf8e5a1fbb02a8de736a83a
André de Queiroz <andrepatrinicola@gmail.com>
2022-07-14 22:17:42 -0300

    Starting to work on models, no migrations made

--- core/models.py
+++ core/models.py
@@ -1,14 +1,13 @@
 from email.mime import image
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
-'''
 class Categorias(models.Model):
     titulo_categoria = models.CharField(max_length=20, verbose_name="titulo da categoria")
     descricao_categoria = models.CharField(max_length=100, verbose_name="descrição da categoria")
     imagem_categoria = models.ImageField()
-
 
 
 
@@ -18,4 +17,4 @@ class Posts(models.Model):
     sub_titulo = models.CharField(max_length=100, verbose_name="subtitulo do post")
     post = models.CharField(max_length=1000, verbose_name="post")
     imagem_post = models.ImageField()
-'''
+    categoria = models.ForeignKey(on_delete=CASCADE)
