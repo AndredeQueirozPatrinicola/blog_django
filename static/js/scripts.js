7be33a239ea70f5cbf0ea3acbeca6253f4f730c8
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-14 00:17:08 -0300

    mudancas nos estaticos

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,34 +1,38 @@
 // document.getElementById('editar-perfil').onclick = () => {
 //     const documento = document.getElementById('div-perfil-meio');
 //     documento.innerHTML = "Teste";
-    
+
 // }
 
-if(document.getElementsByTagName('label').length >= 6)
-{
-    const botoesImagem = document.getElementsByTagName('label')
-    if(botoesImagem)
-    {
-        let labelImagem = botoesImagem[3]
-        labelImagem.classList.add('post-button')
-    }
 
-    const selectInput = document.getElementById('id_categoria')
-    if(selectInput)
-    {
-        selectInput.classList.add('select-input')
-    }
-}
-else if(document.getElementsByTagName('label').length === 3)
-{
-    const botoesImagem = document.getElementsByTagName('label')
+if (!document.getElementById('login')) {
+    if (document.getElementsByTagName('label').length >= 6) {
+        const botoesImagem = document.getElementsByTagName('label')
+        if (botoesImagem) {
+            let labelImagem = botoesImagem[3]
+            labelImagem.classList.add('post-button')
+        }
 
-    if(botoesImagem)
-    {
-        let labelImagem = botoesImagem[2]
-        labelImagem.classList.add('post-button')
-        labelImagem.style.width = '170px' 
+        const selectInput = document.getElementById('id_categoria')
+        if (selectInput) {
+            selectInput.classList.add('select-input')
+        }
     }
+    else if (document.getElementsByTagName('label').length === 3) {
+        const botoesImagem = document.getElementsByTagName('label')
+
+        if (botoesImagem) {
+            let labelImagem = botoesImagem[2]
+            labelImagem.classList.add('post-button')
+            labelImagem.style.width = '170px'
+        }
+    }
+}else{
+    const password_1 = document.getElementById('id_password1')
+    const password_2 = document.getElementById('id_password2')
+
+    password_1.classList.add('input-login')
+    password_2.classList.add('input-login')
 }
 
 
